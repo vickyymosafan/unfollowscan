1fc64ea7b3793161d859f040330716464f956367
M. Vicky Mosafan <mvickymosafan@gmail.com>
2025-10-29 13:47:20 +0700

    refactor: wrap Header component in HeaderWrapper for improved layout and styling consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,30 +5,38 @@ import styled from 'styled-components';
 
 export default function Header() {
   return (
-    <ButtonContainer>
-      <Link href="/" className="logo-brand">
-        <img src="/Logo.svg" alt="Logo" className="logo" />
-        <span className="brand-text">Cek Follow Balik</span>
-      </Link>
+    <HeaderWrapper>
+      <ButtonContainer>
+        <Link href="/" className="logo-brand">
+          <img src="/Logo.svg" alt="Logo" className="logo" />
+          <span className="brand-text">Cek Follow Balik</span>
+        </Link>
 
-      <Link href="/">
-        <button className="button" title="Beranda">
-          <svg className="icon" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-            <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z" />
-          </svg>
-        </button>
-      </Link>
+        <Link href="/">
+          <button className="button" title="Beranda">
+            <svg className="icon" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z" />
+            </svg>
+          </button>
+        </Link>
 
-      <Link href="/panduan">
-        <button className="button" title="Panduan">
-          <svg className="icon" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 18H6a1 1 0 0 0 0 2h15v2H6a3 3 0 0 1-3-3V4a2 2 0 0 1 2-2h16v16zM5 16.05c.16-.03.33-.05.5-.05H19V4H5v12.05zM8 8h9v2H8V8zm0 4h9v2H8v-2z" />
-          </svg>
-        </button>
-      </Link>
-    </ButtonContainer>
+        <Link href="/panduan">
+          <button className="button" title="Panduan">
+            <svg className="icon" stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path d="M21 18H6a1 1 0 0 0 0 2h15v2H6a3 3 0 0 1-3-3V4a2 2 0 0 1 2-2h16v16zM5 16.05c.16-.03.33-.05.5-.05H19V4H5v12.05zM8 8h9v2H8V8zm0 4h9v2H8v-2z" />
+            </svg>
+          </button>
+        </Link>
+      </ButtonContainer>
+    </HeaderWrapper>
   );
 }
+
+const HeaderWrapper = styled.div`
+  background-color: colorrgb(246, 246, 246);
+  width: 100%;
+  padding: 20px 0;
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -39,8 +47,8 @@ const ButtonContainer = styled.div`
   padding: 0 20px;
   border-radius: 10px;
   box-shadow: rgba(34, 34, 34) 0px 5px 15px, rgb(61, 61, 61) 5px 10px 15px;
-  gap: 15px;
-  margin: 0 auto 20px auto;
+  gap: 5px;
+  margin: 0 auto;
   max-width: fit-content;
 
   .logo-brand {
@@ -55,7 +63,6 @@ const ButtonContainer = styled.div`
 
   .logo-brand:hover {
     transform: translateY(-3px);
-    background-color: rgba(255, 255, 255, 0.1);
   }
 
   .logo {
